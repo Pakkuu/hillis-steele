d55cada62810e046dd668f0b2de0f25639e3d9bc
pakkuu <ethan.pvarghese@gmail.com>
2026-02-27 22:42:45 -0600

    base implementation with output writing

--- my-sum.cpp
+++ my-sum.cpp
@@ -30,7 +30,6 @@ bool read_and_validate_input(std::istream& in, int expected_count, std::vector<i
     return true;
 }
 
-//calculate prefix_sum
 void prefix_sum(std::vector<int>& nums) {
     std::vector<int> result(nums.size());
     std::inclusive_scan(nums.begin(), nums.end(), result.begin());
@@ -38,28 +37,36 @@ void prefix_sum(std::vector<int>& nums) {
     return;
 }
 
-void write_to_file(std::vector<int>& nums) {
-
+void write_to_file(std::ostream& out, std::vector<int>& nums) {
+    for (size_t i = 0; i < nums.size(); i++) {
+        if (i > 0) {
+            out << ' ';
+        }
+        out << nums[i];
+    }
+    return;
 }
 
 int main(int argc, char* argv[]) {
 
+    //open input stream
     std::ifstream in(argv[3]);
     if (!in) {return 1;} //check file exists and can be opened
 
-    int expected_count = std::stoi(argv[1]);
-    std::vector<int> data;
+    int expected_count = std::stoi(argv[1]); //expected # of elements to process
+    std::vector<int> nums;
 
-    if (!read_and_validate_input(in, expected_count, data)) {
+    if (!read_and_validate_input(in, expected_count, nums)) {
         return 1;
     }
 
-    prefix_sum(data);
-    write_to_file(data);
+    prefix_sum(nums);
 
-    for (int x: data) { 
-        std::cout << x << std::endl;
-    }
+    //open output stream
+    std::ofstream out(argv[4]);
+    if (!out) {return 1;}
+
+    write_to_file(out, nums);
 
     return 0;
 }
